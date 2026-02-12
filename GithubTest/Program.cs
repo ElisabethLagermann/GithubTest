3db168d1e6c04cd26d1f9fd86d52fd2170a1e0d4
Elisabeth Holløse Rantzau Lagermann <ell004@edu.zealand.dk>
2026-02-12 11:42:07 +0100

    Tilføjet ny Console.WriteLine

--- GithubTest/Program.cs
+++ GithubTest/Program.cs
@@ -1,2 +1,5 @@
 ﻿
 Console.WriteLine("Jeg skal teste, om jeg kan committe, pulle og pushe til GitHub");
+
+Console.WriteLine("Mit repository er oprettet, og nu skal jeg vise, jeg kan pulle og pushe til GitHub");
+
