914135a217ff05e793ae5dd113d0464f55928167
Elisabeth Holløse Rantzau Lagermann <ell004@edu.zealand.dk>
2026-02-12 11:45:37 +0100

    Forklaring på, hvordan jeg synkroniserer med GitHub inde fra Visual Studio

--- GithubTest/Program.cs
+++ GithubTest/Program.cs
@@ -3,3 +3,12 @@ Console.WriteLine("Jeg skal teste, om jeg kan committe, pulle og pushe til GitHu
 
 Console.WriteLine("Mit repository er oprettet, og nu skal jeg vise, jeg kan pulle og pushe til GitHub");
 
+
+/* For at synkronisere med min/gruppens repository på GitHub, skal jeg klikke på følgende:
+ - View
+- Git Changes
+- Commit all (husk først at skrive en message, der forklarer, hvilke ændringer jeg har foretaget).
+- Derefter vælg de to pile, der følger hinanden i en cirkel "Sync (Pull then Push)"
+- Så kan de nye ændringer ses inde på HitHun*/
+
+
